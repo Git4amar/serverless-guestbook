a895eee42a6ce95c2c100343fef53df9806f7ca0
Git4amar <103874177+Git4amar@users.noreply.github.com>
2023-01-11 19:33:57 -0800

    Added endpoints and url constant

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/40d9e79d-a3b9-41f4-9caa-c87a7c16e587/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.json,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
